32928351be0be6b496fae6b47e90c0ba5fef7554
cparsons <cparsons@thoughtmachine.net>
2024-04-04 11:16:42 +0100

    Disable mouse selection being passed to vim

--- lua/set.lua
+++ lua/set.lua
@@ -7,7 +7,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -18,7 +17,7 @@ vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.tildeop = true 
+vim.opt.tildeop = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -32,3 +31,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.shada = "!,'5000,<500,s10,h"
+
+-- Prevent passing mouse to vim when ssh
+vim.opt.mouse = ""
