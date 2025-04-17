9ddf4528200b33e1b44d667365b1fbff3f4b79ba
Chris Parsons <cparsons@thoughtmachine.net>
2025-04-17 10:04:59 +0100

    move flash back to just 's'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,8 +11,8 @@ return {
         opts = {},
         -- stylua: ignore
         keys = {
-            { "gs",    mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "gS",    mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
             { "gr",    mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
             { "gR",    mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
@@ -102,13 +102,13 @@ return {
             },
         },
     },
-    {
-        dir = "~/Documents/eof-eol-marker.vim",
-        name = "eof-marker",
-        config = function()
-            require('eof-marker').setup()
-        end
-    },
+    -- {
+    --     dir = "~/Documents/eof-eol-marker.vim",
+    --     name = "eof-marker",
+    --     config = function()
+    --         require('eof-marker').setup()
+    --     end
+    -- },
     {
         "cuducos/yaml.nvim",
         ft = { "yaml" },
--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,13 +1,13 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-		vim.cmd([[
-	let $NVIM_MKDP_LOG_FILE = expand('~/mkdp-log.log')
-	let $NVIM_MKDP_LOG_LEVEL = 'debug'
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+        vim.cmd([[
+	let $NVIM_MKDP_LOG_FILE = expand('/tmp/mkdp-log.log')
+	let $NVIM_MKDP_LOG_LEVEL = 'info'
 	]])
-	end,
+    end,
 }
