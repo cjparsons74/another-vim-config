34d36c247cbb1b7f98631193740744c25e33a86b
Chris Parsons <cparsons@thoughtmachine.net>
2025-06-24 09:21:59 +0100

    Cleaner mini.files handling and config explorer

--- lua/keys.lua
+++ lua/keys.lua
@@ -9,6 +9,24 @@ vim.cmd([[
     nmap <LocalLeader>h 0/http<CR>"ayW:silent exec "!xdg-open\ '<C-R>a'"<CR>:redraw!<CR>
 ]])
 
-vim.keymap.set("n", "<Leader>e", ":cd %:h<CR>:lua MiniFiles.open()<CR>", { desc = "Explore" })
+vim.keymap.set('n', '<Leader>e', function()
+    local file = vim.api.nvim_buf_get_name(0)
+    local path
+    if file ~= '' then
+        -- If there is a file, get its directory
+        path = vim.fn.fnamemodify(file, ':h')
+    else
+        -- If no file, use current working directory
+        path = vim.fn.getcwd()
+    end
+    require('mini.files').open(path)
+end, { desc = 'Open MiniFiles at current file or cwd' })
+
 vim.keymap.set("n", "<Leader>qq", ":wqa<CR>", { desc = "Write and quit" })
 vim.keymap.set("n", "<Leader>qq", ":wqa<CR>", { desc = "Write and quit" })
+vim.keymap.set('n', '<Leader>fx', function()
+    -- Get the directory containing your init.lua/init.vim
+    local config_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ':h')
+    -- Open MiniFiles at that directory
+    require('mini.files').open(config_dir)
+end, { desc = 'Open MiniFiles at config folder' })
