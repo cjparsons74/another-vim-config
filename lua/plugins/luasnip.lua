eff82e882958f3bc56c374e9d542e32eb8202bf8
Chris Parsons <chris@cjp.email>
2026-04-18 07:11:03 +0100

    Add snippents

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -25,9 +25,9 @@ return {
 <head>
     <meta charset="utf-8">
     <style>
-        body {{ margin: 0; padding: 0 !important; background-color: #faf6f1; }}
-        table, td {{ border-collapse: collapse; }}
-        p {{ margin: 0; padding: 0; margin-bottom: 1em; }}
+        body {{{{ margin: 0; padding: 0 !important; background-color: #faf6f1; }}}}
+        table, td {{{{ border-collapse: collapse; }}}}
+        p {{{{ margin: 0; padding: 0; margin-bottom: 1em; }}}}
     </style>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700" rel="stylesheet">
 </head>
@@ -38,19 +38,109 @@ return {
             <tr>
                 <td><img src="https://www.bethesdachildren.org/static/newsletter/Header600.png" width="600" style="width: 100%; height: auto"></td>
             </tr>
-            {}
+            <!-- content here -->
+            <tr>
+                <td align="center"
+                    style="background-color: #ffffff;
+                           padding: 20px 0;
+                           font-size: 0;
+                           text-align: center">
+                    <table role="presentation"
+                           border="0"
+                           cellspacing="0"
+                           cellpadding="0"
+                           style="width: 100%;
+                                  max-width: 186px;
+                                  display: inline-block;
+                                  vertical-align: top">
+                        <tr>
+                            <td align="center"
+                                width="186"
+                                style="width: 186px;
+                                       text-align: center;
+                                       padding: 10px;
+                                       font-size: 14px;
+                                       font-family: Nunito, sans-serif">
+                                <a href="mailto:chris.parsons@bethesdachildren.org"
+                                   style="display: block;
+                                          color: #000000;
+                                          text-decoration: underline">Contact us</a>
+                            </td>
+                        </tr>
+                    </table>
+                    <table role="presentation"
+                           border="0"
+                           cellspacing="0"
+                           cellpadding="0"
+                           style="width: 100%;
+                                  max-width: 186px;
+                                  display: inline-block;
+                                  vertical-align: top">
+                        <tr>
+                            <td align="center"
+                                width="186"
+                                style="width: 186px;
+                                       text-align: center;
+                                       padding: 10px;
+                                       font-size: 14px;
+                                       font-family: Nunito, sans-serif">
+                                <a href="https://www.bethesdachildren.org"
+                                   style="display: block;
+                                          color: #000000;
+                                          text-decoration: underline">Website</a>
+                            </td>
+                        </tr>
+                    </table>
+                    <table role="presentation"
+                           border="0"
+                           cellspacing="0"
+                           cellpadding="0"
+                           style="width: 100%;
+                                  max-width: 186px;
+                                  display: inline-block;
+                                  vertical-align: top">
+                        <tr>
+                            <td align="center"
+                                width="186"
+                                style="width: 186px;
+                                       text-align: center;
+                                       padding: 10px;
+                                       font-size: 14px;
+                                       font-family: Nunito, sans-serif">
+                                <a href="{{{{{{ RESEND_UNSUBSCRIBE_URL }}}}}}"
+                                   style="display: block;
+                                          color: #000000;
+                                          text-decoration: underline">Unsubscribe</a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 5px 10px;
+                           font-family: Nunito, sans-serif;
+                           font-size: 11pt;
+                           line-height: 1.4;
+                           background-color: #ffffff;
+                           text-align: center;
+                           color: #888888">
+                    <p>
+                        <small>Registred UK charity no. 1153449. Registered as a non-profit company 8321768.</small>
+                    </p>
+                </td>
+            </tr>
         </table>
     </center>
 </body>
 </html>
             ]],
-						{ i(1, "News from the Metty home"), i(0) }
+						{ i(1, "News from the Metty home") }
 					)
 				),
 
-				-- 2. IMAGE + TEXT ROW
+				-- 2. 1/3 IMAGE + TEXT ROW
 				s(
-					"news-img-txt",
+					"news-third-img-txt",
 					fmt(
 						[[
                 <tr>
@@ -67,6 +157,62 @@ return {
                             <tr>
                                 <td style="padding: 10px;">
                                     <img alt="{}" src="{}" width="160" style="width: 100%; border-radius: 10px;">
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            ]],
+						{ i(1, "Headline"), i(2, "Content"), i(3, "Alt Text"), i(4, "URL") }
+					)
+				),
+				-- 2b. 1/2 IMAGE + TEXT ROW
+				s(
+					"news-half-img-txt",
+					fmt(
+						[[
+                    <tr>
+                    <td style="padding: 10px;
+                               background-color: #ffffff;
+                               text-align: center;
+                               font-size: 0">
+                        <table border="0"
+                               cellpadding="0"
+                               cellspacing="0"
+                               width="100%"
+                               style="max-width: 300px;
+                                      display: inline-block;
+                                      vertical-align: middle;
+                                      text-align: left">
+                            <tr>
+                                <td style="font-family: Nunito, sans-serif;
+                                           font-size: 16px;
+                                           line-height: 1.5;
+                                           color: #111111;
+                                           padding: 10px">
+                                    <h4 style="margin: 0 0 10px 0;">{}</h4>
+                                    <p style="margin: 0;">
+                                    {}
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                        <table border="0"
+                               cellpadding="0"
+                               cellspacing="0"
+                               width="100%"
+                               style="max-width: 280px;
+                                      display: inline-block;
+                                      vertical-align: middle">
+                            <tr>
+                                <td style="padding: 30px 10px;">
+                                    <img alt="{}"
+                                         src="https://www.bethesdachildren.org/static/newsletter/{}"
+                                         width="260"
+                                         newsletter.png
+                                         style="width: 100%;
+                                                border-radius: 10px;
+                                                display: block">
                                 </td>
                             </tr>
                         </table>
@@ -99,18 +245,153 @@ return {
 					fmt(
 						[[
                 <tr>
-                    <td style="padding: 20px; background-color: #ffffff;">
-                        <table width="100%" style="background-color: #22c55e; border-radius: 8px; color: #ffffff;">
-                            <tr>
-                                <td style="padding: 20px; font-family: Nunito, sans-serif;">
-                                    <p style="margin: 0;">{} <a href="{}" style="color: #ffffff; text-decoration: underline;">{}</a></p>
+                <td style="padding: 0 20px; background-color: #ffffff;">
+                    <table width="100%"
+                           style="background-color: #22c55e;
+                                  border-radius: 8px;
+                                  color: #ffffff">
+                        <tr>
+                            <td style="padding: 20px; font-family: Nunito, sans-serif;">
+                                                <p style="margin: 0;">{}
+                                                <a href="{}" style="color: #ffffff; text-decoration: underline;">{}</a></p>
+                                            </td>
+                                        </tr>
+                                    </table>
                                 </td>
+                            </tr>
+            ]],
+						{ i(1, "Message"), i(2, "URL"), i(3, "Link Text") }
+					)
+				),
+				-- Video
+				s(
+					"news-video",
+					fmt(
+						[[
+            <tr>
+                <td style="padding: 15px 30px;
+                           font-family: Nunito, sans-serif;
+                           background-color: #ffffff;
+                           text-align: center">
+                    <p style="margin: 0;">
+                        <a href="https://www.bethesdachildren.org/static/newsletter/{}"
+                           target="_blank"
+                           style="text-decoration: none">
+                            <img width="300"
+                                 alt="Oscar theo video thumbnail"
+                                 src="https://www.bethesdachildren.org/static/newsletter/{}"
+                                 style="display: block;
+                                        margin: 0 auto;
+                                        border: 0">
+                        </a>
+                    </p>
+                </td>
+            </tr>
+                    ]],
+						{ i(1, "alt"), i(2, "filename") }
+					)
+				),
+				-- Signoff
+				s(
+					"news-signoff",
+					fmt(
+						[[
+                    <tr>
+                    <td style="padding: 15px 30px;
+                               font-family: Nunito, sans-serif;
+                               background-color: #ffffff">
+                        <p style="margin: 0 0 10px 0;">
+                        {}
+                        </p>
+                        <p style="margin: 0 0 10px 0;">
+                        {}
+                        </p>
+                        <p style="margin: 0;">
+                            Many thanks,
+                            <br>
+                            BCF Trustees
+                        </p>
+                    </td>
+                </tr>
+                    ]],
+						{ i(1, "p1"), i(2, "p2") }
+					)
+				),
+				-- Two pictures with captions
+				s(
+					"news-two-pictures-captions",
+					fmt(
+						[[
+                    <tr>
+                    <td style="padding: 0px;
+                               background-color: #ffffff;
+                               text-align: center;
+                               font-size: 0">
+                        <table border="0"
+                               cellpadding="0"
+                               cellspacing="0"
+                               width="100%"
+                               style="max-width: 280px;
+                                      display: inline-block;
+                                      vertical-align: top">
+                            <tr>
+                                <td style="padding: 5px 5px 0 5px;">
+                                    <img alt="{}"
+                                         src="https://www.bethesdachildren.org/static/newsletter/{}"
+                                         width="260"
+                                         style="width: 100%;
+                                                border-radius: 10px;
+                                                display: block">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family: Nunito, sans-serif;
+                                           font-size: 13px;
+                                           line-height: 1.4;
+                                           color: #666666;
+                                           text-align: center;
+                                           padding: 5px 10px 15px 10px;
+                                           font-style: italic">{}</td>
+                            </tr>
+                        </table>
+                        <table border="0"
+                               cellpadding="0"
+                               cellspacing="0"
+                               width="100%"
+                               style="max-width: 280px;
+                                      display: inline-block;
+                                      vertical-align: top">
+                            <tr>
+                                <td style="padding: 5px 5px 0 5px;">
+                                    <img alt="{}"
+                                         src="https://www.bethesdachildren.org/static/newsletter/{}"
+                                         width="260"
+                                         style="width: 100%;
+                                                border-radius: 10px;
+                                                display: block">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family: Nunito, sans-serif;
+                                           font-size: 13px;
+                                           line-height: 1.4;
+                                           color: #666666;
+                                           text-align: center;
+                                           padding: 5px 10px 15px 10px;
+                                           font-style: italic">{}</td>
                             </tr>
                         </table>
                     </td>
                 </tr>
-            ]],
-						{ i(1, "Message"), i(2, "URL"), i(3, "Link Text") }
+                    ]],
+						{
+							i(1, "alt1"),
+							i(2, "filename1"),
+							i(3, "caption1"),
+							i(4, "alt2"),
+							i(5, "filename2"),
+							i(6, "caption2"),
+						}
 					)
 				),
 			})
